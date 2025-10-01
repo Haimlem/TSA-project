0db3e217fb1b27ce4ed65f00d51d1c2c93935bdc
convitcon224 <dungbu2000@gmail.com>
2025-10-01 16:26:09 +0700

    format GUI

--- GUI.py
+++ GUI.py
@@ -12,7 +12,7 @@ class GUI:
     def render(self):
         self.gui = tk.Tk()
         self.gui.resizable(TRUE,TRUE)
-        # self.ControlWindow.iconbitmap(LOGO_PATH)
+        self.gui.iconbitmap("assets/TSA.ico")
         self.gui.title("TSA")
         self.gui.geometry("450x420")
         self.gui.configure(bg=self.BG_COLOR)
@@ -50,20 +50,24 @@ class GUI:
             font=('Comic Sans MS', 12, 'bold'),
             foreground=self.BG_COLOR
         )
-        ttk.Style().map(
-            'Custom.TCombobox',
-            foreground=[('readonly', self.FG_COLOR)],
-            selectforeground=[('readonly', self.FG_COLOR)],
-        )
+
+        style = ttk.Style()
+        style.theme_use("clam")
+        style.configure("TNotebook.Tab",
+                background=self.BG_COLOR,
+                foreground=self.FG_COLOR)
+        style.map("TNotebook.Tab",
+                background=[("active", "lightblue"), ("disabled", "gray"), ("selected", "#5089f2")],
+                foreground=[("active", "black"), ("disabled", "gray")])
 
         # ---------- EXIT ----------
         self.gui.protocol("WM_DELETE_WINDOW",self.onDestroy)
         self.gui.mainloop()
 
     # ---------- SECTION 1 ----------
-    def build_section1(self, parent):
+    def build_section1(self, parent: tk.Frame):
         frame = tk.Frame(parent, bg=self.BG_COLOR)
-        frame.pack(padx=20, pady=20, anchor="w")
+        frame.pack(padx=20, pady=20)
 
         tk.Label(frame, text="Input", bg=self.BG_COLOR,
                  fg=self.FG_COLOR, font=("Arial", 15, "bold")).grid(row=0, column=0, sticky="w")
@@ -72,15 +76,15 @@ class GUI:
         tk.Label(frame, text="L : Weight (하중)", bg=self.BG_COLOR,
                  fg=self.FG_COLOR, font=("Arial", 14)).grid(row=1, column=0, sticky="w")
         tk.Entry(frame, textvariable=self.weight1,
-                 font=("Arial", 14), width=5).grid(row=1, column=1)
+                 font=("Arial", 14), width=5).grid(row=1, column=1, padx=(10,0))
         tk.Label(frame, text="kg", bg=self.BG_COLOR, fg=self.FG_COLOR,
-                 font=("Arial", 14)).grid(row=1, column=2)
+                 font=("Arial", 14)).grid(row=1, column=2, padx=(5,0))
 
         self.numWires1 = IntVar()
         tk.Label(frame, text="N : Number of wires (줄의 수)", bg=self.BG_COLOR,
                  fg=self.FG_COLOR, font=("Arial", 14)).grid(row=2, column=0, sticky="w")
         tk.Entry(frame, textvariable=self.numWires1,
-                 font=("Arial", 14), width=5).grid(row=2, column=1)
+                 font=("Arial", 14), width=5).grid(row=2, column=1, padx=(10,0))
         
         tk.Label(frame, text="Lifetime Prediction", bg=self.BG_COLOR,
                  fg=self.FG_COLOR, font=("Arial", 15, "bold")).grid(row=3, column=0, sticky="w")
@@ -94,13 +98,13 @@ class GUI:
                                 fg=self.FG_COLOR, font=("Arial", 14))
         self.output1.grid(row=6, column=0, sticky="w", columnspan=3)
 
-        ttk.Button(frame, text="Calculate", command=self.calculate1).grid(
-            row=7, column=0, pady=10)
+        ttk.Button(frame, text="Calculate", command=self.calculate1, width=12).grid(
+            row=7, column=0, pady=10, columnspan = 3)
 
     # ---------- SECTION 2 ----------
-    def build_section2(self, parent):
+    def build_section2(self, parent: tk.Frame):
         frame = tk.Frame(parent, bg=self.BG_COLOR)
-        frame.pack(padx=20, pady=20, anchor="w")
+        frame.pack(padx=20, pady=20)
 
         tk.Label(frame, text="Input", bg=self.BG_COLOR,
                  fg=self.FG_COLOR, font=("Arial", 15, "bold")).grid(row=0, column=0, sticky="w")
@@ -109,23 +113,23 @@ class GUI:
         tk.Label(frame, text="L : Weight (하중)", bg=self.BG_COLOR,
                  fg=self.FG_COLOR, font=("Arial", 14)).grid(row=1, column=0, sticky="w")
         tk.Entry(frame, textvariable=self.weight2,
-                 font=("Arial", 14), width=5).grid(row=1, column=1)
+                 font=("Arial", 14), width=5).grid(row=1, column=1, padx=(10,0))
         tk.Label(frame, text="kg", bg=self.BG_COLOR, fg=self.FG_COLOR,
-                 font=("Arial", 14)).grid(row=1, column=2)
+                 font=("Arial", 14)).grid(row=1, column=2, padx=(5,0), sticky="w")
 
         self.diameter2 = DoubleVar()
         tk.Label(frame, text="D : Diameter (직경)", bg=self.BG_COLOR,
                  fg=self.FG_COLOR, font=("Arial", 14)).grid(row=2, column=0, sticky="w")
         tk.Entry(frame, textvariable=self.diameter2,
-                 font=("Arial", 14), width=5).grid(row=2, column=1)
+                 font=("Arial", 14), width=5).grid(row=2, column=1, padx=(10,0))
         tk.Label(frame, text="mm", bg=self.BG_COLOR, fg=self.FG_COLOR,
-                 font=("Arial", 14)).grid(row=2, column=2)
+                 font=("Arial", 14)).grid(row=2, column=2, padx=(5,0), sticky="w")
 
         self.numWires2 = IntVar()
         tk.Label(frame, text="N : Number of wires (줄의 수)", bg=self.BG_COLOR,
                  fg=self.FG_COLOR, font=("Arial", 14)).grid(row=3, column=0, sticky="w")
         tk.Entry(frame, textvariable=self.numWires2,
-                 font=("Arial", 14), width=5).grid(row=3, column=1)
+                 font=("Arial", 14), width=5).grid(row=3, column=1, padx=(10,0))
 
         tk.Label(frame, text="Lifetime Prediction", bg=self.BG_COLOR,
                  fg=self.FG_COLOR, font=("Arial", 15, "bold")).grid(row=4, column=0, sticky="w")
@@ -139,8 +143,8 @@ class GUI:
                                 fg=self.FG_COLOR, font=("Arial", 14))
         self.output2.grid(row=7, column=0, sticky="w", columnspan=3)
 
-        ttk.Button(frame, text="Calculate", command=self.calculate2).grid(
-            row=8, column=0, pady=10)
+        ttk.Button(frame, text="Calculate", command=self.calculate2, width=12).grid(
+            row=8, column=0, pady=10, columnspan=3)
 
     def calculate1(self):
         k = 2.83 * pow(10,5)
